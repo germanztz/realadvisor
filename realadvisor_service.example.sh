4e9570a7c37d710d979c0498d0118b161957ae01
daimler <11229630+germanztz@users.noreply.github.com>
2025-03-02 09:12:20 +0100

    prepare prod

--- realadvisor_service.example.sh
+++ realadvisor_service.example.sh
@@ -5,4 +5,4 @@ APP_PATH=$HOME/workspaces/realadvisor
 
 cd $APP_PATH
 source $APP_PATH/.venv/bin/activate
-$APP_PATH/.venv/bin/python $APP_PATH/src/daemon.py --start & 
+$APP_PATH/.venv/bin/python $APP_PATH/src/daemon.py --start 
